cfddb8346df5219749c8bda620fb474c24eca305
frengkiiskandar <frankierockabilly@gmail.com>
2026-08-07 21:39:12 +0700

    oke

--- src/data/payment.js
+++ src/data/payment.js
@@ -1,6 +1,6 @@
 export const payments = [
   {
-    invoice: "INV-20260807-001-Enggal Setiawan",
+    invoice: "INV-20260807-001-EnggalSetiawan",
     client: "Enggal Setiawan",
     project: "Skripsi Manajemen",
     date: "07 Agustus 2026",
@@ -10,7 +10,7 @@ export const payments = [
   },
 
   {
-    invoice: "INV-20260807-002-Sulisiani Nufus",
+    invoice: "INV-20260807-002-SulisianiNufus",
     client: "Sulistiani Nufus",
     project: "Sistem Pakar - Certainty Factor",
     date: "07 Agustus 2026",
@@ -20,7 +20,7 @@ export const payments = [
   },
 
   {
-    invoice: "INV-20260807-003-Tri Widodo",
+    invoice: "INV-20260807-003-TriWidodo",
     client: "Client Ketiga",
     project: "Landing Page",
     date: "07 Agustus 2026",
